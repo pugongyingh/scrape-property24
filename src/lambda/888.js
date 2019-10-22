140a85cbcb7b6dea4a62d1e97be9ab19c951ce1d
pugongyingh <pgyhh@sina.cn>
2019-10-22 21:38:41 +0800

    Update 888.js

--- src/lambda/888.js
+++ src/lambda/888.js
@@ -3,49 +3,31 @@ const cheerio = require('cheerio');
 
 exports.handler = function (event, context, callback) {
 
-    const url = 'https://www.indeed.ca/jobs?q=pharmacist&l=Milton&radius=40';
+    const url = 'http://202.118.167.76/ksap/all.asp';
+    const query = {
+        url,
+        method: 'POST',
+        data: `keyword=${stuid}&ok=%B2%E9%D5%D2`,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        responseType: 'arraybuffer',
+        transformResponse: gbkTransform,
+        proxy: {
+            host: 'neauproxy.feit.me',
+            port: 6000,
+        }
+    };
+
+    let result;
+
+    result = await axios.request(query);
+    
+     send(result.data);
+    
+
 
     // get data from url
     // then send it
-    const test = () => {
-        axios.get(url)
-            .then(async (res) => {                
-                if (res.status === 200) {
-                    const html = res.data;
-                    const $ = cheerio.load(html);                    
-                    let result = [];
 
-                    $('td#resultsCol div.result').each(await function (i, elem) {
-                        let rec = {}                         
-                        let id = $(this).attr('id');
-                        let jts = $(this).find('a'); 
-                        let jt = $(jts[0]).text().trim();
-                        let jl = 'https://www.indeed.ca' + $(jts[0]).attr('href').trim();
-                        let co = $(`#${id} > div > div:nth-child(1) > span`).text().trim();
-                        let lo = $(`#${id} > div > span`).text().trim(); 
-
-                        if(lo === '') {
-                            console.log(`[${i + 1}]loc retry`);
-                            lo = $(`#${id} > div.sjcl > div.location`).text().trim(); 
-                        }
-
-                        //console.log(`[${i + 1}]${jt} co:*${co}* lo:*${lo}*`);
-
-                        rec.jobTitle = jt;
-                        rec.jobLink = jl;
-                        rec.company = co;
-                        rec.location = lo;
-                        result.push(rec);
-
-                    });
-                  
-                    send(html);
-                }
-
-
-            })
-            .catch(err => send(err));
-    }
 
     // send response
     // note data is passed in response body
